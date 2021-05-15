377367d31348955afceee540d32ad0aed0c729e9
任岳峰 <ryfds123git>
2021-05-15 10:34:23 +0800

    默认图片地址

---
+++ navigation-web/src/main/java/com/pzc/navigationweb/constant/ImgConstants.java
@@ -0,0 +1,10 @@
+package com.pzc.navigationweb.constant;
+
+/**
+ * @author ryf
+ * @date 5/15/21 10:04 AM
+ */
+public class ImgConstants {
+
+    public static final String DEFAULT_PICTURE = "http://jzm-oss.oss-cn-hangzhou.aliyuncs.com/cloudStorage/1620897480145";
+}
--- navigation-web/src/main/java/com/pzc/navigationweb/service/impl/NavigationResourcesServiceImpl.java
+++ navigation-web/src/main/java/com/pzc/navigationweb/service/impl/NavigationResourcesServiceImpl.java
@@ -1,11 +1,13 @@
 package com.pzc.navigationweb.service.impl;
 
 import cn.hutool.core.date.DateUtil;
+import com.alibaba.dubbo.common.utils.StringUtils;
 import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.pzc.navigationweb.common.util.InitDOUtil;
 import com.pzc.navigationweb.common.util.Result;
+import com.pzc.navigationweb.constant.ImgConstants;
 import com.pzc.navigationweb.dao.NavigationResourcesDOMapper;
 import com.pzc.navigationweb.domain.dbdo.NavigationResourcesDO;
 import com.pzc.navigationweb.domain.mapstruct.NavigationReqToDo;
@@ -29,6 +31,10 @@ public class NavigationResourcesServiceImpl implements NavigationResourcesServic
     @Override
     public Result<Boolean> submit(NavigationResourcesReqDTO navigationResourcesReqDTO) {
         Result<Boolean> result = new Result<>();
+        // 设置默认值
+        if (StringUtils.isNotEmpty(navigationResourcesReqDTO.getLogoUrl())) {
+            navigationResourcesReqDTO.setLogoUrl(ImgConstants.DEFAULT_PICTURE);
+        }
         NavigationResourcesDO navigationResourcesDO = NavigationReqToDo.INSTANCE.reqToDo(navigationResourcesReqDTO);
         InitDOUtil.initField(navigationResourcesDO);
         result.setModule(navigationResourcesDOMapper.insertSelective(navigationResourcesDO) > 0);
@@ -62,10 +68,10 @@ public class NavigationResourcesServiceImpl implements NavigationResourcesServic
         NavigationResourcesDO resourcesDO = navigationResourcesDOMapper.selectByPrimaryKey(id);
         resourcesDO.setOpenCount(resourcesDO.getOpenCount()+1);
         result.setSuccess(navigationResourcesDOMapper.updateByPrimaryKey(resourcesDO) > 0);
+
         NavigationResourcesRespDTO respDTO = NavigationReqToDo.INSTANCE.doToResp(resourcesDO);
         respDTO.setCreateDateStr(DateUtil.formatDateTime(respDTO.getCreateDate()));
         result.setModule(respDTO);
-
         return result;
     }
 }
