636ca787268326932cc3c4de3f4f656d69eb6f17
任岳峰 <ryfds123git>
2021-06-17 18:58:08 +0800

    获取当前电脑的mac地址

--- navigation-web/src/main/java/com/pzc/navigationweb/service/impl/NavigationResourcesServiceImpl.java
+++ navigation-web/src/main/java/com/pzc/navigationweb/service/impl/NavigationResourcesServiceImpl.java
@@ -20,7 +20,13 @@ import com.pzc.navigationweb.dto.respdto.NavigationResourcesRespDTO;
 import com.pzc.navigationweb.service.NavigationResourcesService;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author ryf
@@ -109,5 +115,37 @@ public class NavigationResourcesServiceImpl implements NavigationResourcesServic
         result.setSuccess(true);
         result.setModule(respDTO);
         return result;
+    }
+
+    public static void main(String[] args) throws UnknownHostException, SocketException {
+        InetAddress ia = InetAddress.getLocalHost();
+        System.out.println(ia);
+        getLocalMac(ia);
+    }
+
+    private static void getLocalMac(InetAddress ia) throws SocketException {
+        // TODO Auto-generated method stub
+        //获取网卡，获取地址
+        byte[] mac = NetworkInterface.getByInetAddress(ia).getHardwareAddress();
+        System.out.println("mac数组长度："+mac.length);
+        StringBuffer sb = new StringBuffer("");
+        for(int i=0; i<mac.length; i++) {
+            if(i!=0) {
+                sb.append(":");
+            }
+            //字节转换为整数
+            int temp = mac[i]&0xff;
+            String str = Integer.toHexString(temp);
+            System.out.println("每8位:"+str);
+            if(str.length()==1) {
+                sb.append("0"+str);
+            }else {
+
+                sb.append(str);
+            }
+        }
+        System.out.println("本机MAC地址:"+sb.toString().toUpperCase());
+
+
     }
 }
