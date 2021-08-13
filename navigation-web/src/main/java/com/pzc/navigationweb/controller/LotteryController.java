ede41e892f1d49c94b9d99358d15dc10c4a33030
任岳峰 <ryfds123git>
2021-08-13 11:25:01 +0800

    彩票号码记录

---
+++ navigation-web/src/main/java/com/pzc/navigationweb/controller/LotteryController.java
@@ -0,0 +1,45 @@
+package com.pzc.navigationweb.controller;
+
+import com.pzc.navigationweb.application.process.AddCategoryProcess;
+import com.pzc.navigationweb.application.process.QueryCategoryListProcess;
+import com.pzc.navigationweb.application.process.RemoveCategoryProcess;
+import com.pzc.navigationweb.common.util.Result;
+import com.pzc.navigationweb.dto.reqdto.CategoryReqDTO;
+import com.pzc.navigationweb.dto.respdto.CategoryRespDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * @author ryf
+ * @date 5/8/21 3:41 PM
+ */
+@RestController
+@RequestMapping(LotteryController.PAGE_NAME)
+public class LotteryController extends BaseController {
+    public static final String PAGE_NAME = "/lottery";
+
+    @Autowired
+    private QueryCategoryListProcess queryCategoryListProcess;
+    @Autowired
+    private AddCategoryProcess addCategoryProcess;
+    @Autowired
+    private RemoveCategoryProcess removeCategoryProcess;
+
+    @RequestMapping("/addCustomLotteryNumber")
+    public Result<List<CategoryRespDTO>> getCategoryList(CategoryReqDTO categoryReqDTO) {
+        return queryCategoryListProcess.start(categoryReqDTO);
+    }
+
+    @RequestMapping("/addCategory")
+    public Result<CategoryRespDTO> addCategory(CategoryReqDTO categoryReqDTO) {
+        return addCategoryProcess.start(categoryReqDTO);
+    }
+
+    @RequestMapping("/removeCategory")
+    public Result<Boolean> removeCategory(CategoryReqDTO categoryReqDTO) {
+        return removeCategoryProcess.start(categoryReqDTO);
+    }
+}
--- navigation-web/src/test/java/com/pzc/navigationweb/NavigationWebApplicationTests.java
+++ navigation-web/src/test/java/com/pzc/navigationweb/NavigationWebApplicationTests.java
@@ -1,5 +1,7 @@
 package com.pzc.navigationweb;
 
+import cn.hutool.core.util.URLUtil;
+import cn.hutool.http.HttpUtil;
 import com.github.pagehelper.PageInfo;
 import com.pzc.navigationweb.common.util.RedisUtil;
 import com.pzc.navigationweb.common.util.Result;
@@ -16,6 +18,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @SpringBootTest(classes = NavigationWebApplication.class)
 @ImportResource("classpath:spring-application.xml")
@@ -56,6 +67,44 @@ class NavigationWebApplicationTests {
         RedisUtil.op().setV("name","ryf");
         System.out.println((String) RedisUtil.op().getV("name"));
 
+    }
+
+    @Test
+    void PaChong(){
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        String path = "/Users/orange_r/Desktop/DLT.txt";
+        String url = "http://kaijiang.500.com/shtml/dlt/21092.shtml?0_ala_baidu";
+        String content = HttpUtil.get(url);
+        File file = new File(path);
+        OutputStreamWriter osw = new OutputStreamWriter(new FileOutputStream(file), "UTF-8");
+        osw.write(content);
+        osw.close();
+
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file), "UTF-8"));
+
+        String regexDate = "<strong>\\d*</strong>";
+        String regexNum = "<td>\\d{1,2}(\\s\\d+)*.\\d{1,2}\\s\\d+</td>";
+        List<String> regexList = new ArrayList<>();
+        regexList.add(regexDate);
+        regexList.add(regexNum);
+        String buf ;
+        while ((buf = br.readLine()) != null) {
+
+            String finalBuf = buf;
+            regexList.stream().forEach(x -> {
+                Pattern proInfo = Pattern.compile(x, Pattern.DOTALL);
+                Matcher buf_m = proInfo.matcher(finalBuf);
+                if (buf_m.find()) {
+                    String bugGroup = buf_m.group();
+                    System.out.println(bugGroup.substring(bugGroup.indexOf('>')+1,
+                            bugGroup.lastIndexOf('<')));
+                }
+            });
+        }
     }
 
 }
