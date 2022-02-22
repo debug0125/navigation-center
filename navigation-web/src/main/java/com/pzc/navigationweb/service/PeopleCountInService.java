b0f064107e00d930ab2e1bfee59048e8867ced2f
任岳峰 <ryfds123git>
2022-02-22 14:50:57 +0800

    使用mybatisPlus， 优化 mapstruct 使用方式

--- navigation-web/src/main/java/com/pzc/navigationweb/service/PeopleCountInService.java
+++ navigation-web/src/main/java/com/pzc/navigationweb/service/PeopleCountInService.java
@@ -1,9 +1,7 @@
 package com.pzc.navigationweb.service;
 
 
-import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.pzc.navigationweb.common.util.Page;
-import com.pzc.navigationweb.domain.dbdo.CountThemeDO;
 import com.pzc.navigationweb.dto.query.CountThemeQuery;
 import com.pzc.navigationweb.dto.respdto.CountThemeRespDTO;
 
